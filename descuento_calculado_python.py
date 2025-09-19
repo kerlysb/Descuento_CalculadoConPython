7a912bcb78d70d747be92eb536adb50263db7531
kerly suarez <kerly_suarezb@hotmail.com>
2025-09-19 10:59:32 -0500

    Descuento con porcetaje personalizado

--- descuento_calculado_python.py
+++ descuento_calculado_python.py
@@ -7,3 +7,6 @@ monto_2 = 200.0
 # Llamada con porcentaje por defecto (10%)
 descuento_1 = calcular_descuento(monto_1)
 total_a_pagar_1 = monto_1 - descuento_1
+# Llamada con porcentaje personalizado (15%)
+descuento_2 = calcular_descuento(monto_2, 15)
+total_a_pagar_2 = monto_2 - descuento_2
