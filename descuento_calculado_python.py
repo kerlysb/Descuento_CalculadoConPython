b5dff5f35eba28ae47179c7e28934dfa80cb8789
kerly suarez <kerly_suarezb@hotmail.com>
2025-09-19 10:57:23 -0500

    Creación de función

--- descuento_calculado_python.py
+++ descuento_calculado_python.py
@@ -1,3 +1,6 @@
 def calcular_descuento(monto_total, porcentaje_descuento=10):
     descuento = monto_total * (porcentaje_descuento / 100)
     return descuento
+# Programa principal
+monto_1 = 150.0
+monto_2 = 200.0
