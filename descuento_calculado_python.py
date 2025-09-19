d383844bfbe50290038e7c6c703c3b4ff3c8bd31
kerly suarez <kerly_suarezb@hotmail.com>
2025-09-19 10:57:53 -0500

    Descuento con porcetaje

--- descuento_calculado_python.py
+++ descuento_calculado_python.py
@@ -4,3 +4,6 @@ def calcular_descuento(monto_total, porcentaje_descuento=10):
 # Programa principal
 monto_1 = 150.0
 monto_2 = 200.0
+# Llamada con porcentaje por defecto (10%)
+descuento_1 = calcular_descuento(monto_1)
+total_a_pagar_1 = monto_1 - descuento_1
